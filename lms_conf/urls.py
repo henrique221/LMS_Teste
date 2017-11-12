ab47288bdd0733688405220f873f0ddeda7ea8d3
Unknown <hborges9294@gmail.com>
2017-11-11 23:52:57 -0200

    ok

--- lms_conf/urls.py
+++ lms_conf/urls.py
@@ -3,21 +3,23 @@ Definition of urls for django_get_started.
 """
 
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from lms_app import views
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
-# from django.contrib import admin
+from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = (
     # Examples:
-    url(r'^$', 'lms_app.views.home', name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home')
+)
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+
