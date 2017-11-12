3ebcdd15d8424ee6682518ffc0dd0b003c0145a2
Unknown <hborges9294@gmail.com>
2017-11-11 22:07:22 -0200

    ok

---
+++ lms_app/models.py
@@ -0,0 +1,25 @@
+"""
+Definition of models.
+"""
+
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    periodo = models.CharField(max_length=50)
+    instituicao = models.CharField(max_length=200)
+
+class Vestibular(models.Model):
+    nome = models.CharField(max_length=200)
+
+
+class Candidato(models.Model):
+    nome = models.CharField(max_length=200)
+    rg = models.IntegerField()
+    cpf = models.IntegerField()
+    endereco = models.CharField(max_length=200)
+    telefone = models.IntegerField()
+
+
+
